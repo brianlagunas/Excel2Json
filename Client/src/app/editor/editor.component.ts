6b7227390e320b14f32c133fa250f25966517300
Yogesh <39595733+dubeyyogesh10@users.noreply.github.com>
2021-08-28 00:21:31 +0530

    Update editor.component.ts

--- Client/src/app/editor/editor.component.ts
+++ Client/src/app/editor/editor.component.ts
@@ -5,63 +5,58 @@ import { FileStorageService } from '../services/file-storage.service';
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
 })
 export class EditorComponent implements OnInit {
+  editorOptions = { theme: 'vs-dark', language: 'javascript', readOnly: true };
+  fileName: string = 'New File';
+  code: string = '[]';
 
-  editorOptions = {theme: 'vs-dark', language: 'javascript', readOnly: true};
-  fileName: string = "New File";
-  code: string = "[]";
-
-  constructor(private fileStorage: FileStorageService) {
-
-  }
+  constructor(private fileStorage: FileStorageService) {}
 
   ngOnInit() {
-    if(this.fileStorage.file){
+    if (this.fileStorage.file) {
       this.fileName = this.fileStorage.file.name;
       this.loadFile(this.fileStorage.file);
     }
   }
 
   loadFile(file: File) {
-    const fileExtension = file.name.split(".").pop();
+    const fileExtension = file.name.split('.').pop();
 
-    if (fileExtension === "csv") {
-      CSV.loadCsvFile(file).then(json => {
+    if (fileExtension === 'csv') {
+      CSV.loadCsvFile(file).then((json) => {
         this.code = json;
       });
-    }
-    else {
+    } else {
       //todo: load excel
     }
   }
 
   onDownloadJsonClicked() {
-    var a = document.createElement("a");
-    a.download = "excel2json-" + Date.now() + ".json";
+    var a = document.createElement('a');
+    a.download = 'excel2json-' + Date.now() + '.json';
+    console.log('console .log 0 ' + this.code);
     const blob = new Blob([this.code], {
-      type: "text/json;charset=utf-8"
+      type: 'text/json;charset=utf-8',
     });
     a.href = window.URL.createObjectURL(blob);
     a.click();
   }
 
   onGetLinkClicked() {
-    const url = "https://localhost:44307/api/share";
+    const url = 'https://localhost:44307/api/share';
 
     const params = {
       headers: {
-        "content-type": "application/json; charset=utf-8"
+        'Content-Type': 'application/json',
       },
-      body: this.code,
-      method: "POST"
-    }
+      body: JSON.stringify(this.code),
+      method: 'POST',
+    };
 
-    fetch(url, params).then( result => {
+    fetch(url, params).then((result) => {
       console.log(result);
     });
-
   }
-
 }
